6132d369d75d0d900ff5ad1cd6f8b042e008d12c
dqh999 <dqhit999@gmail.com>
2025-01-05 00:29:55 +0700

    update

--- src/app/order/[id]/status/page.tsx
+++ src/app/order/[id]/status/page.tsx
@@ -1,19 +1,15 @@
-import React from 'react'
+export default async function OrderStatusPage({ 
+  params 
+}: { 
+  params: Promise<{ id: string }> 
+}) {
+  const { id } = await params
 
-interface PageProps {
-  params: {
-    id: string
-  }
-}
-
-export default async function OrderStatusPage({ params }: PageProps) {
-  const { id } = params
 
   return (
     <div>
       <h1>Order Status</h1>
       <p>Order ID: {id}</p>
-      {/* Add more content here */}
     </div>
   )
 }
